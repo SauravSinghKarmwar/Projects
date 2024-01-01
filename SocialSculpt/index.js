2ab0232b91ef52bac73bdf6398956d96bd271622
[SauravSinghKarmwar] <[sauravchp2@gmail.com]>
2024-01-01 10:03:06 +0530

    # Added-searchMessages-feature-using-Js

--- SocialSculpt/index.js
+++ SocialSculpt/index.js
@@ -4,20 +4,26 @@ const menuItems = document.querySelectorAll(".menu-item");
 // MESSAGES
 const messagesNotification = document.querySelector("#messages-notifications");
 const messages = document.querySelector(".messages");
+const message = messages.querySelectorAll(".message");
+const messageSearch = document.querySelector("#message-search");
 
-// remove active class for all menu items
+// Remove active class for all menu items
 const changeActiveItem = () => {
     menuItems.forEach((item) => {
         item.classList.remove("active");
     });
 };
 
-// add active class for the clicked item
+// Add active class for the clicked item
 menuItems.forEach((item) => {
     item.addEventListener("click", () => {
+        // Remove active class from all menu items
         changeActiveItem();
+
+        // Add active class to the clicked item
         item.classList.add("active");
 
+        // Get references to notifications popup and notification count element
         const notificationsPopup = document.querySelector(
             ".notifications-popup"
         );
@@ -25,6 +31,7 @@ menuItems.forEach((item) => {
             "#notifications .notification-count"
         );
 
+        // Toggle display of notifications popup and notification count
         if (item.id !== "notifications") {
             notificationsPopup.style.display = "none";
             notificationCount.style.display = "block";
@@ -38,13 +45,42 @@ menuItems.forEach((item) => {
     });
 });
 
-// Messages
+// ================ MESSAGES ===============
+
+// Search chat
+const searchMessage = () => {
+    const val = messageSearch.value.toLowerCase();
+    console.log(val);
+
+    // Iterate through each message card
+    message.forEach((chat) => {
+        // Get the lowercase name from the h5 element
+        let name = chat.querySelector("h5").textContent.toLowerCase();
+
+        // Check if the search value is present in the name
+        if (name.indexOf(val) !== -1) {
+            chat.style.display = "flex";
+        } else {
+            chat.style.display = "none";
+        }
+    });
+};
+
+// Search chat
+messageSearch.addEventListener("keyup", searchMessage);
+
+// Highlight messages card when messages menu item is clicked
 messagesNotification.addEventListener("click", () => {
+    // Add a temporary box shadow to the messages container when messages menu item is clicked
     messages.style.boxShadow = "0 0 1rem var(--color-primary)";
+
+    // Hide the notification count after clicking on the messages notifications
     messagesNotification.querySelector(".notification-count").style.display =
         "none";
 
+    // Remove the box shadow after a delay
     setTimeout(() => {
         messages.style.boxShadow = "none";
     }, 2000);
 });
+
